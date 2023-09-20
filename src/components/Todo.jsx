5c73db41241a8bc83003013e16d49b92dc64a2af
givsty <golovkovviggreserv@gmail.com>
2023-09-20 22:14:02 +0300

    fix input

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -9,23 +9,23 @@ const Todo = () => {
     "Sports",
     "+New category",
   ];
-  const [todos, setTodos] = useState([{ name: "Построить беседку", active: false}]);
-  const task = (e) =>{
-    setTodos(todos.concat({ name: e.target.value , active: false}));
-  }
+
+  const [todos, setTodos] = useState([{ name: "Построить беседку", completed: false}]);
+  
+  const [input, setInput] = useState('');
+
+  const task = e => setTodos(todos.concat([{ name: e.target.value , completed: false}]));
+
   const addTask = (e) => {
-    if (e.key === "Enter") {
-      setTodos(todos.concat({ name: e.target.value , active: false}));
+    if (e.key === "Enter" && e.target.value !== "" && todos.length !== 8 && e.target.value !== 0) {
+      task(e)
     }
   };
-  const handleKeyPress = (event) => {
-    if(event.key === "Enter") {
-      console.log('work')
-    }
-  }
+
   const setActive = (event) => {
-    event.active = true
-  }
+    event.active = true;
+  };
+
   return (
     <div className="wrapper">
       <div className="content">
@@ -39,15 +39,29 @@ const Todo = () => {
           <div className="line"></div>
           <div className="categories-header">
             <h2>All Task</h2>
-            <input type="text" onKeyDown={addTask} placeholder="Add new task" onKeyDownCapture={handleKeyPress}/>
-            <button className="send" >Отправить</button>
+            <input
+              type="text"
+              onChange={(e) => setInput(e.target.value)}
+              onKeyUpCapture={addTask}
+              placeholder="Add new task"
+              value={todos.name}
+            />
+            <button className="send" onClick={(e) => {
+              if(todos.length !== 8 && e.target.value !== 0) {
+                setTodos(todos.concat([{ name: input }]))
+              }
+            }}>
+              Отправить
+            </button>
             <div className="categories-content">
               {todos.map((element) => {
                 return (
-                  <li style={{textDecoration: element.active ? 'line-through' : ''}}>
-                    <input type="checkbox"
-                    onClick={(e) => setActive}
-                     />
+                  <li
+                    style={{
+                      textDecoration: element.active ? "line-through" : "",
+                    }}
+                  >
+                    <input type="checkbox" onClick={(e) => setActive} />
                     {element.name}
                   </li>
                 );
