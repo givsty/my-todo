d868261866c22540e8c52e298ef77e76144d804f
givsty <golovkovviggreserv@gmail.com>
2023-09-26 22:52:54 +0300

    fix input

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import del from '../assets/img/delete.png';
 
 const Todo = () => {
   const categories = [
@@ -10,22 +11,28 @@ const Todo = () => {
     "+New category",
   ];
 
-  const [todos, setTodos] = useState([{ name: "Построить беседку", completed: false}]);
-  
+  const [todos, setTodos] = useState([{ name: "Построить беседку", completed: false, id: Math.floor(Math.random() * 100)}]);
+
   const [input, setInput] = useState('');
 
-  const task = e => setTodos(todos.concat([{ name: e.target.value , completed: false}]));
+  const task = () => setTodos(todos.concat([{ name: input, completed: false }]));;
 
   const addTask = (e) => {
-    if (e.key === "Enter" && e.target.value !== "" && e.target.value !== '') {
+    if (e.key === "Enter" && input !== "" && input !== '') {
       task(e)
+      setInput('')
     }
   };
 
-  const setActive = (event) => {
-    event.active = true;
+  const setActive = (id) => { 
+    setTodos(todos.filter((todo) =>{
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    }))
   };
-
+  console.log(todos);
   return (
     <div className="wrapper">
       <div className="content">
@@ -44,11 +51,12 @@ const Todo = () => {
               onChange={(e) => setInput(e.target.value)}
               onKeyUpCapture={addTask}
               placeholder="Add new task"
-              value={todos.name}
+              value={input}
             />
             <button className="send" onClick={(e) => {
-              if(todos.length !== 8 && e.target.value !== '') {
-                setTodos(todos.concat([{ name: input }]))
+              if(input) {
+                task(e)
+                setInput('')
               }
             }}>
               Отправить
@@ -61,8 +69,9 @@ const Todo = () => {
                       textDecoration: element.active ? "line-through" : "",
                     }}
                   >
-                    <input type="checkbox" onClick={(e) => setActive} />
+                    <input type="checkbox" onClick={setActive} />
                     {element.name}
+                    <img src={del} alt="" />
                   </li>
                 );
               })}
