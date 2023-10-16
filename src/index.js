061de1f4132ba98d51af279a248ef11b85028532
givsty <golovkovviggreserv@gmail.com>
2023-10-16 21:41:14 +0300

    try to fix errror 404

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./App.scss";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter , HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
