67c0649c60c291e0ee51d407295d4f4e207bc577
givsty <golovkovviggreserv@gmail.com>
2023-10-21 22:26:26 +0300

    trying fix login and signUp

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import {getAuth, signInWithEmailAndPassword} from 'firebase/auth';
 import Form from '../components/ui/Form';
 import { setUser } from '../store/slices/userSlice';
+import { useDispatch } from 'react-redux';
 
 const Login = () => {
+  const dispatch = useDispatch
   const handleLogin = (email, password) =>{
     const auth = getAuth()
     signInWithEmailAndPassword(auth, email, password)
       .then(({user})=>{
         console.log(user);
+        dispatch(setUser({
+          email: user.email,
+          id: user.uid,
+          token: user.accesToken,
+        }))
       })
       .catch(console.error)
   }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import {getAuth, createUserWithEmailAndPassword} from 'firebase/auth';
 import Form from '../components/ui/Form';
+import { setUser } from '../store/slices/userSlice';
+import { useDispatch } from 'react-redux';
 const SignUp = () => {
-  const handleLogin = (email, password) =>{
+  const dispatch = useDispatch
+  const handleRegister = (email, password) =>{
     const auth = getAuth()
     console.log(auth);
     createUserWithEmailAndPassword(auth, email, password)
-      .then(console.log)
+      .then(({user}) =>{
+        dispatch(setUser({
+          email: user.email,
+          id: user.uid,
+          token: user.accesToken,
+        }))
+      })
       .catch(console.error)
   }
   return (
     <Form 
       title='register'
-      handleClick={handleLogin}
+      handleClick={handleRegister}
     />
   )
 }
